2b5e855cf16403b59096cb58c69da2d02c0fc944
Gnagno <gnagno@gnagno.it>
2011-03-12 03:24:14 +0100

    textbox is working

--- app/controllers/quidgets_controller.rb
+++ app/controllers/quidgets_controller.rb
@@ -13,4 +13,15 @@ class QuidgetsController < ActionController::Base
     render :json => {:msg => "The #{params[:object_model_name]} was updated"}, :layout => false
   end
  
+  def quidgets_textbox_update
+    object=params[:object_model_name].camelize.constantize.find(params[:id])
+    
+    object.update_attribute(params[:method].to_sym, params[:svalue])
+    
+    if object.save
+      render :json => {:msg => "The #{params[:object_model_name]} was updated"}, :layout => false
+    else
+      render :json => {:msg => "Impossible to update #{params[:object_model_name]}"}, :layout => false
+    end
+  end
 end
